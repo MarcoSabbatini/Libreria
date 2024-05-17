3dfe2c4ef75a9cf72c95a113e9935479c145034e
MarcoSabbatini <117950317+MarcoSabbatini@users.noreply.github.com>
2024-05-17 12:02:18 +0200

    Finished ServiceExtensions

--- Extensions/ServiceExtensions.cs
+++ Extensions/ServiceExtensions.cs
@@ -1,16 +1,24 @@
-﻿namespace Libreria.Extensions
-using Libreria.Models.Context;
+﻿using Libreria.Models.Context;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Libreria.Repositories;
 
+
+namespace Libreria.Extensions
 {
     public static class ServiceExtensions
     {
-        public static IServiceCollection AddRepositoryService(this IServiceCollection services, IConfiguration configuration) { 
-    
-        services.AddDbContext<MyDbContext>(config =>
-        {
-            config.UseSq
-        })
+        public static IServiceCollection AddRepositoryService(this IServiceCollection services, IConfiguration configuration) {
+
+            services.AddDbContext<MyDbContext>(config =>
+            {
+                config.UseSqlServer(configuration.GetConnectionString("MyDbContext"));
+            });
+            services.AddScoped<BookRepository>();
+            services.AddScoped<CategoryRepository>();
+            services.AddScoped<UserRepository>();
+            return services;
         }
     }
 }
