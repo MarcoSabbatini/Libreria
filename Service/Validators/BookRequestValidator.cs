6df179fc22b7303ec995c8ce8818e4fb144ce4d0
MarcoSabbatini <117950317+MarcoSabbatini@users.noreply.github.com>
2024-05-18 20:35:57 +0200

    searchvalidation fixed probably forever

--- Service/Validators/BookRequestValidator.cs
+++ Service/Validators/BookRequestValidator.cs
@@ -8,37 +8,49 @@ namespace Libreria.Service.Validators
     public class BookRequestValidator : AbstractValidator<BookRequest>
     {
         public BookRequestValidator() {
-            //controlla se è stato inserito almen(o un filtro tra quelli nell'enum
+            //controlla se è stato inserito almeno un filtro tra quelli nell'enum
             RuleFor(c => c.Filters)
                 .NotEmpty()
                 .IsInEnum()
                 .Custom(SearchingValidation);
-            //    .WithMessage("La ricerca deve avvenire con almeno un filtro applicato");
-            /*
-            RuleFor(c => c.FilterCategories)
-                .Custom(SerchingValidation);*/
         }
 
         private void SearchingValidation(ICollection<SearchFilters> collection, ValidationContext<BookRequest> context)
         {
-            if(context.InstanceToValidate.Filters.Contains(SearchFilters.CATEGORY) &&
-                context.InstanceToValidate.Category.Length < 1) 
+            foreach (var filter in collection)
             {
-                context.AddFailure("La ricerca usando il filtro Categoria deve contenere almeno un carattere");
-            } else if(context.InstanceToValidate.Filters.Contains(SearchFilters.PUBLISHINGDATE) &&
-                context.InstanceToValidate.PublishingDate.CompareTo(DateTime.Now) >= 0) 
-            {
-                context.AddFailure("La ricerca usando il filtro Data di publblicazione deve riportare una data precedente ad oggi");
-            } else if(context.InstanceToValidate.Filters.Contains(SearchFilters.NAME) &&
-                context.InstanceToValidate.Name.Length < 1)
-            {
-                context.AddFailure("La ricerca usando il filtro Nome deve contenere almeno un carattere");
-            } else if (context.InstanceToValidate.Filters.Contains(SearchFilters.AUTHOR) &&
-                context.InstanceToValidate.Author.Length < 1)
-            {
-                context.AddFailure("La ricerca usando il filtro Autore deve contenere almeno un carattere");
+                switch (filter)
+                {
+                    case SearchFilters.CATEGORY:
+                        if (context.InstanceToValidate.Category.Length < 1)
+                        {
+                            context.AddFailure("La ricerca usando il filtro Categoria deve contenere almeno un carattere");
+                        }
+                        break;
+                    case SearchFilters.PUBLISHINGDATE:
+                        if (context.InstanceToValidate.PublishingDate.CompareTo(DateTime.Now) >= 0)
+                        {
+                            context.AddFailure("La ricerca usando il filtro Data di publblicazione deve riportare una data precedente ad oggi");
+                        }
+                        break;
+                    case SearchFilters.NAME:
+                        if (context.InstanceToValidate.Name.Length < 1)
+                        {
+                            context.AddFailure("La ricerca usando il filtro Nome deve contenere almeno un carattere");
+                        }
+                        break;
+                    case SearchFilters.AUTHOR:
+                        if (context.InstanceToValidate.Author.Length < 1)
+                        {
+                            context.AddFailure("La ricerca usando il filtro Autore deve contenere almeno un carattere");
+                        }
+                        break;
+                    default:
+                        // Ignora filtri non supportati
+                        break;
+                }
             }
-
         }
+
     }
 }
