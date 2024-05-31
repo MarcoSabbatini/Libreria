b46a0536af7db50d144145baedc557eb91b9e8a7
MarcoSabbatini <117950317+MarcoSabbatini@users.noreply.github.com>
2024-05-31 15:42:59 +0200

    Added something in the bookcontroller

--- Api/ServiceExtension.cs
+++ Api/ServiceExtension.cs
@@ -26,7 +26,18 @@ namespace Libreria.Api
                 s.SwaggerDoc("v1", new OpenApiInfo
                 {
                     Title = "Libreria API",
-                    Version = "v1"
+                    Version = "v1",
+                    Description = "An API to manage a Libreria with their resources. ",
+                    Contact = new OpenApiContact 
+                    { 
+                        Name = "Support Team",
+                        Email = "marco01.sabbatini@studenti.unicam.it"
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "MIT License",
+                        Url = new Uri("https://opensource.org/licenses/MIT")
+                    }
                 });
                 s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
                 {
@@ -37,12 +48,14 @@ namespace Libreria.Api
                     In = ParameterLocation.Header,
                     Description = "JWT Authorization header using the Bearer scheme. " +
                     "\r\n\r\n Enter 'Bearer' [space] and then your token in the text input below." +
-                    "\r\n\r\nExample: \"Bearer 1safsfsdfdfd\""
+                    "\r\n\r\nExample: \"Bearer 12345abcdef\""
                 });
                 s.AddSecurityRequirement(new OpenApiSecurityRequirement()
                 {
                     {
-                    new OpenApiSecurityScheme {
+                    new OpenApiSecurityScheme 
+                    {
+
                             Reference = new OpenApiReference
                             {
                                 Type = ReferenceType.SecurityScheme,
@@ -52,6 +65,7 @@ namespace Libreria.Api
                     Array.Empty<string>()
                     }
                 });
+                s.CustomSchemaIds(type => type.FullName);
             });
             return services;
         }
@@ -59,17 +73,17 @@ namespace Libreria.Api
         public static IServiceCollection AddSecurityServices(this IServiceCollection services, IConfiguration configuration)
         {
             var jwtAuthenticationOption = new JwtAuthenticationOption();
-            configuration.GetSection("JwtAuthentication")
-                .Bind(jwtAuthenticationOption);
-            services.AddAuthentication(options => {
+            configuration.GetSection("JwtAuthentication").Bind(jwtAuthenticationOption);
+
+            services.AddAuthentication(options => 
+            {
                 options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
                 options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(options => {
-                string key = jwtAuthenticationOption.Key;
-                var securityKey = new SymmetricSecurityKey(
-                    Encoding.UTF8.GetBytes(key)
-                    );
+            })
+            .AddJwtBearer(options => 
+            {
+                var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtAuthenticationOption.Key));
                 options.TokenValidationParameters = new TokenValidationParameters()
                 {
                     ValidateIssuer = true,
--- Contollers/BookController.cs
+++ Contollers/BookController.cs
@@ -5,6 +5,7 @@ using Libreria.Service.Abstraction;
 using Libreria.Service.Models.Requests;
 using Libreria.Models.Entities.Actions;
 using Libreria.Service.Models.Dtos;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Libreria.Contollers
 {
@@ -22,6 +23,11 @@ namespace Libreria.Contollers
 
         [HttpGet]
         [Route("get")]
+        [SwaggerOperation(
+            Summary = "Get books by a filter",
+            Description = "Fetches all books corresponding to the filters",
+            OperationId = "GetBooks"
+        )]
         public IActionResult GetBooks(BookRequest request) {
             if (_bookService.GetBooks(request).Success) return Ok();
             else return BadRequest();
@@ -29,6 +35,11 @@ namespace Libreria.Contollers
 
         [HttpPost]
         [Route("librarymodifications")]
+        [SwaggerOperation(
+            Summary = "Modifies library",
+            Description = "Modifies library depending on which action is chosen",
+            OperationId = "ModBooks"
+        )]
         public IActionResult LibraryModification(BookDto dto, BookActions action)
         {
             if (_bookService.LibraryModification(dto, action).Success) return Ok();
